0677bcdb42c701b3afef22f35ff301f8c7392707
Vaszheni <Diox@ukr.net>
2021-07-09 00:35:14 +0300

    +туду

--- arena/Arena.py
+++ arena/Arena.py
@@ -1,9 +1,29 @@
 from random import randint
-from human import Knight
+from human.Knight import Knight
 class Arena(Knight):
-    def battle(self, knight1:Knight, knight2:Knight):
-        while knight1.hp() > 0 or knight2.hp() > 0 :
-            if randint(0, 9) < 5 :
-                knight2.hp = knight2.hp() - knight1.power()
+    @staticmethod
+    def battle(knights):
+        #проверка количества участников
+        if len(knights) != 2:
+            print("неверное количество участников")
+            return
+        #проверка на соответсвие рангов
+        if knights[0].rank != knights[1].rank:
+            print("несоответствие рангов")
+            return
+
+        #проведение сражения
+        while knights[0]._health > 0 and knights[1]._health > 0:
+            if randint(0, 9) < 5:
+                knights[1]._health = knights[1]._health - knights[0].power
             else:
-                knight1.hp = knight1.hp() - knight2.power()
+                knights[0]._health = knights[0]._health - knights[1].power
+        else:
+            #вывод и ап победителя
+            for knight in knights:
+                if knight._health > 0:
+                    print(f"Winner are {knight.name}")
+                    knight.rank += 1
+                    if knight.rank > 3:
+                        knight.rank = 3
+                    print(f'{knight.name} rank {knight.rank}')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from human.Knight import Knight
+from arena.Arena import Arena
 
 Arture = Knight(name="Arture", rank=1, health=100, power=10, equip=['sword', 'shield'])
-Barbarossa = Knight(name="Barbarossa", rank=1, health=100, power=8, equip=['sword'])
+Barbarossa = Knight(name="Barbarossa", rank=3, health=100, power=8, equip=['sword'])
+Hexagon = Arena.battle([Arture, Barbarossa])
