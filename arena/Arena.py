4e9aecccd1354bf614db4453bbffc7bc48f23357
Vaszheni <64032579+Vaszheni@users.noreply.github.com>
2021-07-09 14:56:39 +0300

    Update Arena.py
    переписываем слас арена с введением 2 новых методов (1) проверка на 2 учасника в баталии, 2) проверка на соответствие рангов)

--- arena/Arena.py
+++ arena/Arena.py
@@ -9,27 +9,23 @@ class Arena():
         
     def checkingParticipants(knights):#вроверка количества участников
         if len(knights) != 2:
+            print('неверное количество сражающихся')
             return False
         
     def checkingRanks(knights):#проверяем ранги учасников на совпадение
         rank = knights[0].rank #запониманем ранг
         if knights[0].rank != knights[1].rank:
+            print('ранги учасвствующих неравны')
             return false
         else:
             return True
-        
-        
+ 
     @staticmethod
     def battle(knights):#необходимо переделать с учётом имеющихся 2 методов проверки рангов и длинны масива учасников
-        #проверка количества участников
-        if len(knights) != 2:
-            print("неверное количество участников")
-            return
-        #проверка на соответсвие рангов
-        if knights[0].rank != knights[1].rank:
-            print("несоответствие рангов")
-            return
-
+        if checkingParticipants(knights):
+            return #если количество учасников не равно дрвум то выходим с боя
+        if checkingRanks(knights):
+            return #если ранги не совпадают то выходим с боя
         #проведение сражения
         while knights[0]._health > 0 and knights[1]._health > 0:
             if randint(0, 9) < 5: #определяем кто бьёт
